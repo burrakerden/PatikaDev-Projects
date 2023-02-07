08775a5b066dfedf329ee908eae3cae046a4480e
burak <dev.burakerden@gmail.com>
2023-02-07 12:53:55 +0300

    Created Project

---
+++ FlickrClientApp/FlickrClientApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrClientApp
+//
+//  Created by Burak Erden on 7.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
